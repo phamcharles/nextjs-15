488b0cecda02417d611d768fcb62559cec1b65ab
Charles Pham <pham.charles.p@gmail.com>
2025-02-15 15:25:19 -0800

    fixes typescript error

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -3,10 +3,11 @@ import clsx from "clsx";
 import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchLatestInvoices } from "@/app/lib/data";
+import { Invoice } from "@/app/lib/definitions";
 
 export default async function LatestInvoices() {
   // const latestInvoices = await fetchLatestInvoices();
-  const latestInvoices = [];
+  const latestInvoices: Invoice[] = [];
 
   return (
     <div className="flex w-full flex-col md:col-span-4">
