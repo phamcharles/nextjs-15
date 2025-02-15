df052c0d58326267f1fafab455060151837aab4e
Charles Pham <pham.charles.p@gmail.com>
2025-02-15 15:29:36 -0800

    reenables fetching of latest invoices

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -3,11 +3,9 @@ import clsx from "clsx";
 import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchLatestInvoices } from "@/app/lib/data";
-import { Invoice } from "@/app/lib/definitions";
 
 export default async function LatestInvoices() {
-  // const latestInvoices = await fetchLatestInvoices();
-  const latestInvoices: Invoice[] = [];
+  const latestInvoices = await fetchLatestInvoices();
 
   return (
     <div className="flex w-full flex-col md:col-span-4">
